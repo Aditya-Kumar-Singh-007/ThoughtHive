ee0463a64843a663398a197d7146c99c4339e186
Aditya <2604aditya@gmail.com>
2025-12-11 18:26:31 +0530

    added animated background

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import NoteState from "./context/notes/NoteState";
 import WriteNote from "./components/WriteNote";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Ballpit from "./Ballpit";
 
 function App() {
   return (
     <>
-      
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+        background: '#000000'
+      }}>
+        <Ballpit
+          count={60}
+          colors={[0xff6b6b, 0x4ecdc4, 0x45b7d1, 0x96ceb4, 0xfeca57, 0xff9ff3]}
+          gravity={0.015}
+          friction={0.998}
+          wallBounce={0.8}
+          maxVelocity={0.06}
+        />
+      </div>
       <NoteState>
         <Router>
           <Navbar />
